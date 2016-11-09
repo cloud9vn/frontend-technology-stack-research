7254af3876678b2126b5c19cd1d9de269b958a29
TuyenThanh <tuyen.dtt@cloud9-solutions.com>
2016-11-09 16:56:24 +0700

    update Gruntfile.js, pakage.json add

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,77 +1,66 @@
-/*
- * grunt-html-test
- * https://github.com/helloyou2012/grunt-html-test
- *
- * Copyright (c) 2014 学霸
- * Licensed under the MIT license.
- */
-
-'use strict';
-
-module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-	 pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'node_modules/grunt-contrib-jshint/tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-		  
-        jshintrc: 'node_modules/grunt-contrib-jshint/node_modules/jshint/node_modules/exit/.jshintrc'
-      }
-    },
+module.exports = function (grunt) {
 	
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
-    html_test: {
-      default_options: {
-        options: {
+	 // Load the Grunt plugins.
+    require('matchdep')
+        .filterDev('grunt-*')
+        .forEach(grunt.loadNpmTasks);
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'), 
+		cssmin: {
+            sitecss: {
+                options: {
+                    banner: '/* LicenseMgr Minified css file */'
+                }, files: {
+                    'minify/styles/main.min.css': [
+                    'minify/styles/custom.css'
+                    ]
+                    
+                }
+            }
+        }, 
+		uglify: {
+            options: {
+                report: 'min',
+                compress: true,
+                mangle: true
+            },
+            'ng-app-min': {
+                files: {
+                    'minify/scripts/main.min.js': [
+                     'app/scripts/main.js'
+                    ]
+                    
+                }
+            }
+        }, 
+		 uncss: {
+            dist: {
+                src: ['app/index.html'],
+                dest: 'minify/styles/custom.css',
+            }
         },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
-
-  });
-
-  // Actually load this plugin's task(s).
- grunt.loadTasks('node_modules/grunt-contrib-jshint/tasks');
-
- 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'nodeunit', ]);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test','cssmin']);
-
+		
+		jshint: {
+			all: [
+        'Gruntfile.js',
+        'app/main.js'
+		]},
+		
+		csslint: {
+			strict: {
+				options: {
+				import: 2
+				},
+			src: ['app/styles/main.css']
+			}
+		},
+    });
+	
+	//grunt.loadNpmTasks('grunt-contrib-cssmin');
+	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-uncss');
+	
+    // Register the default tasks.
+    grunt.registerTask('default', ['uglify','csslint','uncss', 'cssmin', 'jshint']);
 };
